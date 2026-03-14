934c65ae40d78b8310e730a7990bba3b5f58ea5d
vitr1988 <vitr@list.ru>
2026-03-14 13:24:30 +0400

    fix: ExampleController works now

--- src/main/java/ru/itmo/spring/lesson6/controller/ExampleController.java
+++ src/main/java/ru/itmo/spring/lesson6/controller/ExampleController.java
@@ -3,11 +3,12 @@ package ru.itmo.spring.lesson6.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.actuate.endpoint.SecurityContext;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequiredArgsConstructor
@@ -17,13 +18,14 @@ public class ExampleController {
     private final HttpServletResponse response;
 
     @GetMapping("/ex")
-    public String example(Model model) {
+    public ModelAndView example(Model model) {
         String name = SecurityContextHolder.getContext().getAuthentication().getName();
-//        ModelAndView result = new ModelAndView();
-//        result.setViewName("example");
-//        result.getModel().put("home", "Welcome to project!!");
-//        return result;
-        model.addAttribute("home", request.getParameter("home"));
-        return "example"; // -> templates/index.html
+        ModelAndView result = new ModelAndView();
+        result.setViewName("example");
+        final String text = request.getParameter("text");
+        if (StringUtils.isNotEmpty(text)) {
+            result.getModel().put("welcomeText", text);
+        }
+        return result;
     }
 }
